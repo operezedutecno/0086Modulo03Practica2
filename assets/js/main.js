2c12e3bcd704571c810b042063690534a3e5b538
Osman Pérez <operez@edutecno.com>
2024-08-12 22:54:45 -0400

    Propuesta Práctica

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,56 @@
 function Estudiante(codigo, nombre, valorCredito, nroCreditos, valorRecargo) {
     this.codigo = codigo;
     this.nombre = nombre;
-    this.valor_credito = valorCredito;
-    this.nro_creditos = nroCreditos;
-    this.valor_recargo = valorRecargo;
+    this.valor_credito = Number(valorCredito);
+    this.nro_creditos = Number(nroCreditos);
+    this.valor_recargo = Number(valorRecargo);
 
     this.calcularMonto = function() {
-        var monto = 0;
-        return monto;
+        return (this.valor_credito * this.nro_creditos) + this.valor_recargo;
     }
 }
+
+
+$(document).ready(function() {
+    var estudiantes = [];
+    $("form").submit(function(evento) {
+        evento.preventDefault();
+        var codigo = $("#txt-codigo").val();
+        var nombre = $("#txt-nombre").val();
+        var valorCredito = $("#txt-valor-credito").val();
+        var nroCreditos = $("#txt-nro-creditos").val();
+        var valorRecargo = $("#txt-valor-recargo").val();
+
+        var estudiante = new Estudiante(codigo, nombre, valorCredito, nroCreditos, valorCredito, valorRecargo);
+        estudiantes.push(estudiante)
+        limpiarFormulario()
+        listarInscritos(estudiantes)
+    })
+
+    function limpiarFormulario() {
+        $("#txt-codigo,#txt-nombre,#txt-valor-credito,#txt-nro-creditos,#txt-valor-recargo").val("");
+    }
+
+    function listarInscritos(estudiantes) {
+        $("#tbl-inscritos tbody").html("");
+        var suma = 0;
+        var contador = 0;
+        for (const estudiante of estudiantes) {
+            var monto = estudiante.calcularMonto();
+            suma = suma + monto;
+            contador++;
+            $("#tbl-inscritos tbody").append(`
+                <tr>
+                    <td>${estudiante.codigo}</td>
+                    <td>${estudiante.nombre}</td>
+                    <td class="text-end">${estudiante.valor_credito}$</td>
+                    <td class="text-end">${estudiante.nro_creditos}</td>
+                    <td class="text-end">${estudiante.valor_recargo}$</td>
+                    <td class="text-end">${monto}$</td>
+                </tr>    
+            `);
+        }
+        $("#total").html(`${suma} $`);
+        $("#promedio").html(`${(suma/contador).toFixed(2)} $`);
+    }
+})
